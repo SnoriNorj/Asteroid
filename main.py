1ded99691a5bce30fb574b07bc941d94951801e2
SnoriNorj <jronirons96@gmail.com>
2026-06-03 18:07:29 -1000

    completed game

--- main.py
+++ main.py
@@ -1,26 +1,54 @@
-import pygame
+import pygame, sys
+
+from asteroidfield import AsteroidField 
 
 from constants import (SCREEN_WIDTH, SCREEN_HEIGHT)
 
-from logger import log_state
+from logger import log_state, log_event
 
 from player import Player
 
+from asteroid import Asteroid
+
+from shot import Shot
+
+
+
 def main():
-  
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
+    Asteroid.containers = (asteroids,updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots,drawable,updatable)
+    player = Player (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+    asteroidfield = AsteroidField()
     clock = pygame.time.Clock()
     dt = 0.0
     while True:
         log_state()
-        player.update(dt)
+        updatable.update(dt)
+        for obj in asteroids:
+            if player.collides_with(obj):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+            for ammo in shots:
+                if ammo.collides_with(obj):
+                    log_event("asteroid_shot")
+                    ammo.kill()
+                    obj.split()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black") # using the screen 
-        player.draw(screen)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
--- player.py
+++ player.py
@@ -1,14 +1,23 @@
 import pygame
 
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+
+from constants import ( PLAYER_RADIUS, 
+    LINE_WIDTH, 
+    PLAYER_TURN_SPEED, 
+    PLAYER_SPEED,
+    PLAYER_SHOOT_SPEED,
+    PLAYER_SHOOT_COOLDOWN_SECONDS )
 
 from circleshape import CircleShape
+
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         # 1. call the parent's constructor first 
         super().__init__(x ,y ,PLAYER_RADIUS)
         self.rotation = 0
+        self.timer_cool_down = 0
      # in the Player class
     def rotate(self,dt):
         self.rotation += PLAYER_TURN_SPEED * dt
@@ -34,7 +43,7 @@ class Player(CircleShape):
     
     def update(self, dt: float) -> None:
         keys = pygame.key.get_pressed()
-
+        self.timer_cool_down -= dt
         if keys[pygame.K_a]:
             self.rotate(dt)
         if keys[pygame.K_w]:
@@ -43,3 +52,19 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_d]:
             self.rotate(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+        
+
+    
+    def shoot(self):
+        if self.timer_cool_down > 0:
+            return None
+        else:
+            self.timer_cool_down = PLAYER_SHOOT_COOLDOWN_SECONDS
+            shot = Shot(self.position.x, self.position.y)
+            unit_vector = pygame.Vector2(0, 1)
+            rotated_vector = unit_vector.rotate(self.rotation)
+            rotated_with_speed_vector = rotated_vector * PLAYER_SHOOT_SPEED
+            shot.velocity = rotated_with_speed_vector
+        
